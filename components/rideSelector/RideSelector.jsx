18e5d0a6ec299b36b11143ce2e1540534f16af71
Enow Divine <sirdivine16@gmail.com>
2022-10-23 21:40:41 +0100

    making rides dynamic

--- components/rideSelector/RideSelector.jsx
+++ components/rideSelector/RideSelector.jsx
@@ -1,11 +1,11 @@
-import avatar from "../../assets/rides/avatar.png";
 import Image from "next/image";
 import { FaEthereum } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 const style = {
   wrapper: `h-full flex flex-col`,
   title: `text-gray-500 text-center text-xs py-2 border-b`,
-  carList: `flex flex-col flex-1 overflow-scroll`,
+  carListStyle: `flex flex-col flex-1 overflow-scroll`,
   car: `flex p-3 m-2 items-center border-2 border-white`,
   selectedcar: `border-2 border-black flex p-3 m-2 items-center`,
   carImage: `h-14`,
@@ -16,62 +16,54 @@ const style = {
   price: `mr-[-0.8rem]`,
 };
 
-const carList = [
-  {
-    service: "UberX",
-    iconUrl: avatar,
-    priceMultiplier: 1,
-  },
-  {
-    service: "UberBlack",
-    iconUrl: avatar,
-    priceMultiplier: 1.5,
-  },
-  {
-    service: "UberBlackSuv",
-    iconUrl: avatar,
-    priceMultiplier: 1.5,
-  },
-  {
-    service: "UberSelect",
-    iconUrl: avatar,
-    priceMultiplier: 1.5,
-  },
-  {
-    service: "UberXL",
-    iconUrl: avatar,
-    priceMultiplier: 1.5,
-  },
-];
-
 const basePrice = 1542;
 
 const RideSelector = () => {
+  const [carList, setCarList] = useState([]);
+
+  console.log(carList);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch("/api/db/getRideTypes");
+
+        console.log(response);
+        const data = await response.json();
+        setCarList(data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
   return (
     <div className={style.wrapper}>
       <div className={style.title}>Choose a ride, or swipe up for more</div>
-      <div className={style.carList}>
-        {carList.map((car, index) => (
-          <div className={style.car} key={index}>
-            <Image
-              src={car.iconUrl}
-              width={50}
-              height={50}
-              alt="Car Icon"
-              className={style.carImage}
-            />
-            <div className={style.carDetals}>
-              <div className={style.service}>{car.service}</div>
-              <div className={style.time}>5 mins away</div>
-            </div>
-            <div className={style.priceContainer}>
-              <div className={style.price}>
-                {((basePrice / 10 ** 5) * car.priceMultiplier).toFixed(5)}
-                <FaEthereum />
+      <div className={style.carListStyle}>
+        {Array.isArray(carList)
+          ? carList.map((car, index) => (
+              <div className={style.car} key={index}>
+                <Image
+                  src={car.iconUrl}
+                  width={50}
+                  height={50}
+                  alt="Car Icon"
+                  className={style.carImage}
+                />
+                <div className={style.carDetals}>
+                  <div className={style.service}>{car.service}</div>
+                  <div className={style.time}>5 mins away</div>
+                </div>
+                <div className={style.priceContainer}>
+                  <div className={style.price}>
+                    {((basePrice / 10 ** 5) * car.priceMultiplier).toFixed(5)}
+                    <FaEthereum />
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
+            ))
+          : "null"}
       </div>
     </div>
   );
